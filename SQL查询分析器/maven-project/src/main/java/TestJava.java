d5a404f9bc13c9e6310ae69bfc61250d3345daa0
AlenZhu111 <63092686+AlenZhu111@users.noreply.github.com>
2021-06-11 15:26:48 +0800

    Add files via upload

---
+++ SQL查询分析器/maven-project/src/main/java/TestJava.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class TestJava{
+    public static void main(String[] args) {
+        //创建一个窗口，创建一个窗口
+//        new Connect();
+        new Index("bigdata115.depts.bingosoft.net","22115","user18_db","user18","pass@bingo18");
+    }
+}
